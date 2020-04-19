4da5e4b81369d908f0287ab65c434814828cae56
gabe <gabrielbruck1@gmail.com>
2020-04-19 20:16:42 +0300

    Excercise 3

--- User.py
+++ User.py
@@ -1,31 +1,39 @@
 import csv
 
 
-class File_handler:
+class User:
 
     def __init__(self):
-        self.users = []
+        self.name = str(input("Please enter your username:"))
+        self.password = str(input("Please enter your password:"))
 
-    def load_from_csv(self, *args):
+    def user_auth(name,password):
         try:
-            with open(args[0]) as csv_file:
+            with open("/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv", 'r') as csv_file:
                 csv_reader = csv.reader(csv_file)
+                access = False
+                user_role = ' '
+
                 for row in csv_reader:
-                    users = {
-                        "id": row[0],
-                        "first": row[1],
-                        "last": row[2],
-                        "password": row[3],
-                        "position": row[4],
-                        "salary": row[5],
-                        "role": row[6]
-                    }
-                    self.users.append(users)
+                    if name == row[1] and password == row[3]:
+                            access = True
+                            user_role = str(row[6])
+
+                if access:
+                    print('Access Granted')
+                    print(f'Your role is {user_role}')
+                    return access
+
+                else:
+                    print('Access Denied')
+                    return access
 
         except Exception as error:
             print("There is an error :" + str(error))
 
 
-UserFile = File_handler()
-UserFile.load_from_csv("/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv")
-print(UserFile.users)
+User.__init__(User)
+User.user_auth(User.name, User.password)
+
+
+
