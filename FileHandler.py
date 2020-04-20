c700cd8cc9c49ef2344561ffaac95fb7f6bfeb45
gabe <gabrielbruck1@gmail.com>
2020-04-20 18:38:34 +0300

    Excercise3/4

--- FileHandler.py
+++ FileHandler.py
@@ -4,37 +4,38 @@ from csv import writer
 
 class File_handler:
 
+    def __init__(self):
+        self.list = []
+
     def load_from_csv(self, file_name):
         try:
             with open(file_name, 'r') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
 
                 for line in csv_reader:
-                    print(line)
+                    self.list.append(line)
 
         except Exception as error:
             print("There is an error :" + str(error))
 
     def append_to_csv(self, file_name, data):
         try:
-            with open(file_name, 'r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
 
-                # for row in csv_reader:
-                #     # if row[0] == data_input[0]:
-
+            self.load_from_csv(file_name)
+            for row in self.list:
+                if row.get("user_id") == data[0]:
+                    raise Exception("This ID already exists")
 
             with open(file_name, 'a+', newline='') as write_obj:
                 csv_writer = writer(write_obj)
                 csv_writer.writerow(data)
 
-
         except Exception as error:
             print("There is an error :" + str(error))
 
 
-data_input = [14, 'Tom', 'Knecht', 'password', 'student', 100, 'teacher']
-
-file = File_handler()
-file.append_to_csv("/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv", data_input)
-file.load_from_csv("/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv")
+# data_input = ['22', 'Tom', 'Knecht', 'password', 'student', 100, 'teacher']
+#
+# file = File_handler()
+# file.append_to_csv("/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv", data_input)
+# file.load_from_csv("/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv")
--- User.py
+++ User.py
@@ -1,23 +1,26 @@
 import csv
+from FileHandler import File_handler
 
 
 class User:
 
-    def inputAuth(self):
-        self.name = str(input("Please enter your username:"))
-        self.password = str(input("Please enter your password:"))
+    def __init__(self):
+        self.handler = File_handler
+        # self.name = str(input("Please enter your username:"))
+        # self.password = str(input("Please enter your password:"))
 
-    def user_auth(name,password):
+    def user_auth(self, name, password):
         try:
-            with open("/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv", 'r') as csv_file:
-                csv_reader = csv.reader(csv_file)
-                access = False
-                user_role = ' '
+            self.handler.load_from_csv(self.handler, '/Users/gabrielbruck/Desktop/Python_mini_project/CSV/User.csv')
+            print(self.handler.list)
 
-                for row in csv_reader:
-                    if name == row[1] and password == row[3]:
-                            access = True
-                            user_role = str(row[6])
+            access = False
+            user_role = ' '
+
+            for row in self.handler.list:
+                if name == row[1] and password == row[3]:
+                    access = True
+                    user_role = str(row[6])
 
                 if access:
                     print('Access Granted')
@@ -32,8 +35,5 @@ class User:
             print("There is an error :" + str(error))
 
 
-User.inputAuth(User)
-User.user_auth(User.name, User.password)
-
-
-
+User.__init__(User)
+User.user_auth(User, 'Amir', '12345678')
